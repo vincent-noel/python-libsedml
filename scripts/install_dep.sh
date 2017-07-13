24eca881e4263b0d0109554c069f27b5a826cbd0
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-13 00:45:13 -0300

    Adding python-setuptools dependency

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -3,6 +3,6 @@
 apt-get update
 apt-get upgrade -y
 apt-get install -y subversion git \
-			g++ python-dev cmake swig \
+			g++ python-dev python-setuptools cmake swig \
 			zlib1g-dev libxml2-dev libbz2-dev \
 			libcurl4-openssl-dev
