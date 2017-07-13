ed66be4abb19cf94899f7c9d84e7373e611f2604
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-13 15:28:38 -0300

    pip install seems to work now

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@ from setuptools import setup, find_packages
 from subprocess import call
 from os.path import join, dirname, abspath
 
-cmd = ["/bin/bash", join(join(dirname(abspath(__file__)), "scripts"), "install.sh")]
-print "> Running %s" % " ".join(cmd)
-call(cmd)
+call(["/bin/bash", join(join(dirname(abspath(__file__)), "scripts"), "install.sh")])
 
-setup(name             = "python-libsedml",
+setup(
+      name             = "python-libsedml",
       version          = "0.4.2",
       description      = "LibSEDML Python API",
       long_description = ("libSEDML is a library for reading, writing and "+
       "manipulating SEDML. It is written in ISO C and C++, supports SEDML "+
-      "Levels 1, Version 1-3, and runs on Linux, Microsoft Windows, and Apple "+
-      "MacOS X. For more information about SEDML, please see http://sed-ml.org/."),
+      "Levels 1, Version 1-3, and runs on Debian Linux For more information"+
+      "about SEDML, please see http://sed-ml.org/."),
       license          = "LGPL",
       author           = "Frank T. Bergmann, Vincent Noel (packaging)",
       url              = "http://sed-ml.org/",
-	packages=find_packages(),
-	include_package_data=True,
+      packages=find_packages(),
+      include_package_data=True
 )
