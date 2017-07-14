6b1bcf5e6753909575a04bd62bdef30a8d997aff
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-14 11:41:04 -0300

    Adding cmake 3.8 install

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -3,6 +3,11 @@
 apt-get update
 apt-get upgrade -y
 apt-get install -y subversion git \
-			g++ python-dev python-setuptools cmake swig \
+			g++ python-dev python-setuptools swig \
 			zlib1g-dev libxml2-dev libbz2-dev \
 			libcurl4-openssl-dev
+
+
+wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz
+tar -zxf  cmake-3.8.2-Linux-x86_64.tar.gz -C /opt
+ln -s /opt/cmake-3.8.2-Linux-x86_64/bin/cmake /usr/bin
