e68f22e4116c9c0384a5c8834bc6fb48b0ef8915
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-14 12:16:22 -0300

    Only updating cmake for version < 3.0.2

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -8,7 +8,13 @@ apt-get install -y subversion git wget \
 			libcurl4-openssl-dev
 
 
-wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz
-tar -zxf  cmake-3.8.2-Linux-x86_64.tar.gz -C /opt
-mv /usr/bin/cmake /usr/bin/cmake_old
-ln -s /opt/cmake-3.8.2-Linux-x86_64/bin/cmake /usr/bin
+# Installing cmake update if cmake version < 3.0.2
+currentver="$(cmake --version | head -n1 | cut -d" " -f 3)"
+requiredver="3.0.2"
+if [ "$(printf "$requiredver\n$currentver" | sort -V | head -n1)" == "$requiredver" ]; then
+    wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz
+    tar -zxf  cmake-3.8.2-Linux-x86_64.tar.gz -C /opt
+    mv /usr/bin/cmake /usr/bin/cmake_old
+    ln -s /opt/cmake-3.8.2-Linux-x86_64/bin/cmake /usr/bin
+fi
+
