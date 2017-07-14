94c8fcc1233c32e3dac6be12075595f62203f0bb
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-14 15:56:26 -0300

    Fixing the condition for updating cmake

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -11,7 +11,7 @@ apt-get install -y subversion git wget \
 # Installing cmake update if cmake version < 3.0.2
 currentver="$(cmake --version | head -n1 | cut -d" " -f 3)"
 requiredver="3.0.2"
-if [ "$(printf "$requiredver\n$currentver" | sort -V | head -n1)" == "$requiredver" ]; then
+if [ "$(printf "$requiredver\n$currentver" | sort -V | head -n1)" != "$requiredver" ]; then
     wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz
     tar -zxf  cmake-3.8.2-Linux-x86_64.tar.gz -C /opt
     mv /usr/bin/cmake /usr/bin/cmake_old
