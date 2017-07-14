bd199eddad44bce8febb1ebabd0a75c88945f536
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-14 17:07:49 -0300

    Added fedora installation

---
+++ scripts/install_dep_fedora.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+dnf install subversion git wget \
+            gcc-c++ python2-devel python2-setuptools swig  \
+            libxml2-devel zlib-devel bzip2-devel libcurl-devel
+
+# Installing cmake update if cmake version < 3.0.2
+currentver="$(cmake --version | head -n1 | cut -d" " -f 3)"
+requiredver="3.0.2"
+if [ "$(printf "$requiredver\n$currentver" | sort -V | head -n1)" != "$requiredver" ]; then
+    wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz
+    tar -zxf  cmake-3.8.2-Linux-x86_64.tar.gz -C /opt
+    mv /usr/bin/cmake /usr/bin/cmake_old
+    ln -s /opt/cmake-3.8.2-Linux-x86_64/bin/cmake /usr/bin
+fi
+
