ffd905b07996ff615d3ad323dbafce056f840029
vincent <vincent.noel@butantan.gov.br>
2017-09-07 13:26:33 -0300

    Fixing swig install

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -6,7 +6,7 @@ apt-get install -y subversion git wget \
 			g++ python-dev python-setuptools swig cmake \
 			zlib1g-dev libxml2-dev libbz2-dev \
 			libcurl4-openssl-dev \
-            libpcre3-dev #For swig 3.0
+			libpcre3-dev
 
 # Installing cmake update if cmake version < 3.0.2
 currentver="$(cmake --version | head -n1 | cut -d" " -f 3)"
@@ -23,7 +23,12 @@ currentver="$(swig -version | head -n 2 | tail -n 1 | cut -d" " -f3)"
 requiredver="3.0.0"
 if [ "$(printf "$requiredver\n$currentver" | sort -V | head -n1)" != "$requiredver" ]; then
     wget https://downloads.sourceforge.net/project/swig/swig/swig-3.0.12/swig-3.0.12.tar.gz
-    tar -zxf swig-3.0.12.tar.gz -C /opt
+    tar -zxf swig-3.0.12.tar.gz
+    cd swig 3.0.12
+    ./configure
+    make
+    make install
+    cd ..
     rm /usr/bin/swig
     ln -s /usr/local/bin/swig /usr/bin/
 fi
