237299bad50fb731f6205d02b6784bf8dd0dadd7
vincent <vincent.noel@butantan.gov.br>
2017-09-08 10:54:51 -0300

    Trying swig 3.0.8 for now

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -5,8 +5,7 @@ apt-get upgrade -y
 apt-get install -y subversion git wget \
 			g++ python-dev python-setuptools swig cmake \
 			zlib1g-dev libxml2-dev libbz2-dev \
-			libcurl4-openssl-dev \
-			libpcre3-dev
+			libcurl4-openssl-dev
 
 # Installing cmake update if cmake version < 3.0.2
 currentver="$(cmake --version | head -n1 | cut -d" " -f 3)"
@@ -18,13 +17,14 @@ if [ "$(printf "$requiredver\n$currentver" | sort -V | head -n1)" != "$requiredv
     ln -s /opt/cmake-3.8.2-Linux-x86_64/bin/cmake /usr/bin
 fi
 
-# Installing swig update if swig version < 3.0.0
+# Installing swig update if swig version < 3.0.8
 currentver="$(swig -version | head -n 2 | tail -n 1 | cut -d" " -f3)"
-requiredver="3.0.0"
+requiredver="3.0.8"
 if [ "$(printf "$requiredver\n$currentver" | sort -V | head -n1)" != "$requiredver" ]; then
-    wget https://downloads.sourceforge.net/project/swig/swig/swig-3.0.12/swig-3.0.12.tar.gz
-    tar -zxf swig-3.0.12.tar.gz
-    cd swig-3.0.12
+    apt-get install -y libpcre3-dev
+    wget https://downloads.sourceforge.net/project/swig/swig/swig-3.0.8/swig-3.0.8.tar.gz
+    tar -zxf swig-3.0.8.tar.gz
+    cd swig-3.0.8
     ./configure
     make
     make install
