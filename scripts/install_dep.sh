0fc692ff2e7433ac346458cd13636fb071471c76
vincent <vincent.noel@butantan.gov.br>
2017-09-21 21:55:10 -0300

    Now forcing cmake==3.9.3

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -7,15 +7,11 @@ apt-get install -y subversion git wget \
 			zlib1g-dev libxml2-dev libbz2-dev \
 			libcurl4-openssl-dev
 
-# Installing cmake update if cmake version < 3.0.2
-currentver="$(cmake --version | head -n1 | cut -d" " -f 3)"
-requiredver="3.0.2"
-if [ "$(printf "$requiredver\n$currentver" | sort -V | head -n1)" != "$requiredver" ]; then
-    wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz
-    tar -zxf  cmake-3.8.2-Linux-x86_64.tar.gz -C /opt
-    mv /usr/bin/cmake /usr/bin/cmake_old
-    ln -s /opt/cmake-3.8.2-Linux-x86_64/bin/cmake /usr/bin
-fi
+
+wget https://cmake.org/files/v3.9/cmake-3.9.3-Linux-x86_64.tar.gz
+tar -zxf  cmake-3.9.3-Linux-x86_64.tar.gz -C /opt
+mv /usr/bin/cmake /usr/bin/cmake_old
+ln -s /opt/cmake-3.9.3-Linux-x86_64/bin/cmake /usr/bin
 
 SWIG_VERSION=$1
 # Forcing swig_version to $SWIG_VERSION
